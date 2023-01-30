920fdf743fb3f3606b4fc21456de7b2e9ae5dbf3
vsl-dev <vusalrehimli149@gmail.com>
2023-01-30 18:15:14 +0400

    vsl-dev

--- server.js
+++ server.js
@@ -21,10 +21,13 @@ app.use("/uploads", express.static("uploads")); // Uploaded files
 // App
 
 app.get("/", (req, res) => {
-  // res
-  //   .status(401)
-  //   .json({ code: 401, message: "Access Denied!", developer: "vsldev.tk" });
-  res.sendFile(__dirname + "/index.html");
+  res.status(401).json({ code: 401, message: "Access denied" });
+});
+
+app.get("/panel", (req, res) => {
+  var key = "vsldev";
+  if (req.query.key !== key) return res.sendFile(__dirname + "/login.html");
+  res.sendFile(__dirname + "/panel.html");
 });
 
 app.use("/upload", require("./routers/upload.js")); // Api routers
