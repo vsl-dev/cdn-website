590a34491888ea41e2fa0039d2ff8bb7f9a8df7c
vsl-dev <vusalrehimli149@gmail.com>
2023-02-26 18:12:31 +0400

    vsl-dev

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const rateLimiter = require("express-rate-limter");
+const rateLimit = require("express-rate-limit");
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -13,6 +13,20 @@ global.db = db;
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 1500,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    code: 429,
+    message:
+      "Too many requests, you have been rate limited. Please try again later.",
+  },
+}); // Rate limiter
+
+app.use(limiter);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,7 +35,11 @@ app.use("/uploads", express.static("uploads")); // Uploaded files
 // App
 
 app.get("/", (req, res) => {
-  res.status(401).json({ code: 401, message: "Access denied" });
+  res.status(200).json({
+    code: 200,
+    message: "Ok",
+    developer: "github.com/vsl-dev",
+  });
 });
 
 app.get("/panel", (req, res) => {
