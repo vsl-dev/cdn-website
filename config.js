c8f98ee28be5fd8b7b770eb0769b26e299ad477a
vsl-dev <vusalrehimli149@gmail.com>
2023-03-06 18:56:23 +0400

    vsl-dev

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   port: 1200, // Server port
   baseURL: "http://localhost:1200", // Base url
   securityLevel: 2, // Security levels | Level 0: Security system is disabled | Level 1: Trusted domains are allowed to access | Level 2: Domain must be trusted domain and have access key
-  trustedDomains: ["https://example.com"], // Trusted domain list
+  trustedDomains: ["https://example.com", "http://localhost:1200"], // Trusted domain list
   authKey: "vsldev.tk", // Access key for uploading
   uploadOnly: ["image/"], // You can use 'video/' and 'audio/' for other types of files or type ['all'] for all type of files .exe, .txt and more
   convertImagesTo: "webp", // You can change the type of the image with this and make them all the same. You can set the format you want, such as jpg, jpeg or png, if you do not want the format of the file to change, just type 'default'
